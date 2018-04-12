95c590804bf60f8d6d3e65c54ab8cb44db888bcd
CharlotteHonG <hunandy14@gmail.com>
2018-04-12 11:04:22 +0800

    完成向上偏移導致bug，引入柏松的混合圖看結果。

--- WarpPerspective/WarpCyli/WarpCyli.cpp
+++ WarpPerspective/WarpCyli/WarpCyli.cpp
@@ -18,6 +18,22 @@ using namespace std;
 #include "Pyramid.hpp"
 
 
+//==================================================================================
+// 私有函式
+//==================================================================================
+// 重設 basic_ImgData 大小
+static void resize_basic_ImgData(basic_ImgData &dst, int newW, int newH, int bits) {
+	dst.raw_img.resize(newW*newH*3);
+	dst.width = newW;
+	dst.height = newH;
+	dst.bits = bits;
+};
+// 輸出到 bmp
+static void write_img(basic_ImgData &dst, string name) {
+	Raw2Img::raw2bmp(name, dst.raw_img, dst.width, dst.height);
+};
+//==================================================================================
+
 
 // 圓柱投影座標反轉換
 inline static  void WarpCylindrical_CoorTranfer_Inve(double R,
@@ -85,44 +101,43 @@ void WarpCyliCorner(const basic_ImgData &src, vector<int>& corner) {
 		}
 	}
 }
+
+
 // 混合兩張圓柱
-void WarpCyliMuitBlend(basic_ImgData &dst, const basic_ImgData &src1, const basic_ImgData &src2,
-	int mx, int my) {
-
-	auto resize_basic_ImgData = [](basic_ImgData &dst, int newW, int newH, int bits) {
-		dst.raw_img.resize(newW*newH*3);
-		dst.width = newW;
-		dst.height = newH;
-		dst.bits = bits;
-	};
-	auto write_img = [](basic_ImgData &dst, string name) {
-		Raw2Img::raw2bmp(name, dst.raw_img, dst.width, dst.height);
-	};
-
+void WarpCyliMuitBlend(basic_ImgData &dst, const 
+	basic_ImgData &src1, const basic_ImgData &src2,
+	int mx, int my) 
+{
 	vector<int> corner;
 	WarpCyliCorner(src1, corner);
 
 	int newH=corner[3]-corner[1]-my;
 	int newW=corner[2]-corner[0]+mx;
 
+	// 兩張圖的高度偏差值
+	int myA = my>0? 0:my;
+	int myB = my<0? 0:my;
+
 	// 整張圖
 	resize_basic_ImgData(dst, newW, newH, 24);
-	/*for (int j = 0; j < newH; j++) {
+
+	basic_ImgData all=dst;
+	for (int j = 0; j < newH; j++) {
 		for (int i = 0; i < newW; i++) {
 			// 圖1
 			if (i < corner[2]-corner[0]) {
 				for (int  rgb = 0; rgb < 3; rgb++) {
-					dst.raw_img[(j*dst.width +i) *3+rgb] = src1.raw_img[(((j+my)+corner[1])*src1.width +(i+corner[0])) *3+rgb];
+					all.raw_img[(j*dst.width +i) *3+rgb] = src1.raw_img[(((j+myA)+corner[1])*src1.width +(i+corner[0])) *3+rgb];
 				}
 			}
 			// 圖2
 			if (i >= mx) {
 				for (int  rgb = 0; rgb < 3; rgb++) {
-					dst.raw_img[(j*dst.width +i) *3+rgb] = src2.raw_img[(((j)+corner[1])*src1.width +((i-mx)+corner[0])) *3+rgb];
+					all.raw_img[(j*dst.width +i) *3+rgb] = src2.raw_img[(((j+myB)+corner[1])*src1.width +((i-mx)+corner[0])) *3+rgb];
 				}
 			}
 		}
-	}*/
+	}
 
 	// 重疊區
 	basic_ImgData cut1, cut2;
@@ -134,19 +149,29 @@ void WarpCyliMuitBlend(basic_ImgData &dst, const basic_ImgData &src1, const basi
 			// 圖1
 			if (i < corner[2]-corner[0]-mx) {
 				for (int  rgb = 0; rgb < 3; rgb++) {
-					cut1.raw_img[(j*cut1.width +i) *3+rgb] = src1.raw_img[(((j+my)+corner[1])*src1.width +(i+corner[0]+mx)) *3+rgb];
+					cut1.raw_img[(j*cut1.width +i) *3+rgb] = src1.raw_img[(((j+myA)+corner[1])*src1.width +(i+corner[0]+mx)) *3+rgb];
 				}
 			}
 			// 圖2
 			if (i >= mx) {
 				for (int  rgb = 0; rgb < 3; rgb++) {
-					cut2.raw_img[(j*cut2.width +(i-mx)) *3+rgb] = src2.raw_img[(((j)+corner[1])*src1.width +((i-mx)+corner[0])) *3+rgb];
+					cut2.raw_img[(j*cut2.width +(i-mx)) *3+rgb] = src2.raw_img[(((j+myB)+corner[1])*src1.width +((i-mx)+corner[0])) *3+rgb];
 				}
 			}
 		}
 	}
+	//write_img(cut1, "__cut1.bmp");
+	//write_img(cut2, "__cut2.bmp");
+
+	// 混合圖片
 	basic_ImgData blend;
 	blendImg(blend, cut1, cut2);
+	write_img(blend, "___lapblend.bmp");
+
+	basic_ImgData blend2;
+	Raw2Img::read_bmp(blend2.raw_img, "___posiblend.bmp", &blend2.width, &blend2.height, &blend2.bits);
+	//blend = blend2;
+
 	// 輸出圖片
 #pragma omp parallel for
 	for (int j = 0; j < newH; j++) {
@@ -154,7 +179,7 @@ void WarpCyliMuitBlend(basic_ImgData &dst, const basic_ImgData &src1, const basi
 			// 圖1
 			if (i < mx) {
 				for (int  rgb = 0; rgb < 3; rgb++) {
-					dst.raw_img[(j*dst.width +i) *3+rgb] = src1.raw_img[(((j+my)+corner[1])*src1.width +(i+corner[0])) *3+rgb];
+					dst.raw_img[(j*dst.width +i) *3+rgb] = src1.raw_img[(((j+myA)+corner[1])*src1.width +(i+corner[0])) *3+rgb];
 				}
 			}
 			// 重疊區
@@ -168,7 +193,7 @@ void WarpCyliMuitBlend(basic_ImgData &dst, const basic_ImgData &src1, const basi
 			// 圖2
 			else if (i >= corner[2]-corner[0]) {
 				for (int  rgb = 0; rgb < 3; rgb++) {
-					dst.raw_img[(j*dst.width +i) *3+rgb] = src2.raw_img[(((j)+corner[1])*src1.width +((i-mx)+corner[0])) *3+rgb];
+					dst.raw_img[(j*dst.width +i) *3+rgb] = src2.raw_img[(((j+myB)+corner[1])*src1.width +((i-mx)+corner[0])) *3+rgb];
 				}
 			}
 		}
@@ -206,23 +231,32 @@ void test_WarpCyli_AlphaBlend()
 }
 void test_WarpCyli_MuitBlend()
 {
-	double ft = 672.673, Ax=219, Ay=3;
+	
+	// sc
+	//double ft = 672.673, Ax=219, Ay=3;
+	//string name1 = "sc02.bmp", name2 = "sc03.bmp";
+	// ball
+	string name1 = "ball_01.bmp", name2 = "ball_02.bmp";
+	double ft = 2252.97, Ax = 539, Ay = 37;
 
 	Timer t1;
 	// 讀取影像
 	basic_ImgData img1, dst1;
-	Raw2Img::read_bmp(img1.raw_img, "sc02.bmp", &img1.width, &img1.height, &img1.bits);
-	basic_ImgData img2, dst2;
-	Raw2Img::read_bmp(img2.raw_img, "sc03.bmp", &img2.width, &img2.height, &img2.bits);
+	Raw2Img::read_bmp(img1.raw_img, name1, &img1.width, &img1.height, &img1.bits);
+	basic_ImgData img2, dst2;  
+	Raw2Img::read_bmp(img2.raw_img, name2, &img2.width, &img2.height, &img2.bits);
 
 	// 投影圖1
 	t1.start();
 	WarpCylindrical(dst1, img1, ft);
 	t1.print(" WarpCylindrical");
+	//Raw2Img::raw2bmp("WarpCyliA.bmp", dst1.raw_img, dst1.width, dst1.height);
+
 	// 投影圖2
 	t1.start();
 	WarpCylindrical(dst2, img2, ft);
 	t1.print(" WarpCylindrical");
+	//Raw2Img::raw2bmp("WarpCyliB.bmp", dst2.raw_img, dst2.width, dst2.height);
 
 	// 縫合圖片.
 	basic_ImgData matchImg;
